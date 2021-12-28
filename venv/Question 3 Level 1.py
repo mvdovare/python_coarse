f976225b6a6cba76d7e8f8bff3d10639eb3131b1
mvdovare <mvdovare@gmail.com>
2021-12-28 14:24:42 +0200

    Question 3
    With a given integer number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral #number between 1 and n (both included). and then the program should print the dictionary

---
+++ venv/Question 3 Level 1.py
@@ -0,0 +1,22 @@
+#Question 3
+#Level 1
+
+#Question:
+#With a given integer number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral #number between 1 and n (both included). and then the program should print the dictionary.
+#Suppose the following input is supplied to the program:
+#8
+#Then, the output should be:
+#{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+#Hints:
+#In case of input data being supplied to the question, it should be assumed to be a console input.
+#Consider use dict()
+
+number_2 = input ('enter number - ')
+result_dict = {}
+
+i = 1
+while i <= int(number_2):
+    result_dict.update({i : i*i})
+    i+=1
+print(result_dict)
