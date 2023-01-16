9a1c0109d8e40c515bea91ac1f42fff06e9bd894
mvdovare <mvdovare@gmail.com>
2023-01-16 17:54:22 +0200

    minor updated task_2

--- tasks_during_videolesson_2.py
+++ tasks_during_videolesson_2.py
@@ -10,16 +10,28 @@ print(d)
 
 #########################################################
 
+# Дан список словарей с ключами name и age.
 # Сформировать список имен и возрастов
 
 spisok = [{'name':'Me', 'age':13}, {'name':'Ne', 'age':15},
           {'name':'Ve', 'age':17}]
+
+names = []
+ages = []
+
+for a in spisok:
+    names.append(a['name'])
+    ages.append(a['age'])
+print(names, ages)   
 
 
 #########################################################
 
 # Пользователь вводит имя, фамилию и отчество.
 # Добавить словарь в список, если такого словаря еще нет.
+l = [{'name':'1', 'lastname':'2', 'middlename':'3'},
+     {'name':'4', 'lastname':'5', 'middlename':'6'}
+    ]
 
 name = input('ВВедите имя - ')
 lastname = input('ВВедите фамилию - ')
@@ -27,15 +39,29 @@ middlename = input('ВВедите отчество - ')
 
 d = {'name':name, 'lastname':lastname, 'middlename':middlename}
 
+if d not in l:
+    l.append(d)
 
-
+l
 
 #########################################################
 
 # Дан список словарей с ключами name и age.
 # Найти самого старшего в списке и вывести имя.
 
+# скорей всего это другимм способом надо было сделать
 
+spisok = [{'name':'Me', 'age':13}, {'name':'Ne', 'age':15},
+          {'name':'Ve', 'age':17}]
+
+names = ''
+ages = 0
+
+for a in spisok:
+    if a['age'] > ages:
+        names = a['name']
+        ages = a['age']
+print(names, ages)
 
 
 ###################################
